2540f626cc9912d2160ba6095286855dba76579c
rafal <admin@recone.org>
2024-06-26 19:44:25 +0100

    just polishing "Server online" badge

--- src/App.tsx
+++ src/App.tsx
@@ -96,19 +96,16 @@ export default class App extends Component<any, AppSet> {
                     <div>
                         <img src="../img/img-server-top.png" />
                         <div className="server-time">
-            <div style={{listStyle: "none", color: "greenyellow", margin:"auto", display:"block"}}>
-
-            </div>
                               <ul>
                                 <li>
-                                { this.state.serverState?.state === "Online" ?
-                 <Button variant="link" onClick={()=>this.toggleServerModal()} >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-broadcast" viewBox="0 0 16 16"><path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/></svg>
-                  <span style={{paddingLeft:"5px"}}>Server is {this.state.serverState?.state} 
-                 <Badge bg="danger" style={{marginLeft:"10px"}} pill>{this.state.serverState?.players}</Badge></span>
-                 </Button> : <Button variant="danger">Server is Offline</Button>}
+                                  { this.state.serverState?.state === "Online" ?
+                                  <Link onClick={()=>this.toggleServerModal()} to="#" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-broadcast" viewBox="0 0 16 16"><path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/></svg>
+                                    <span style={{paddingLeft:"5px"}}>Server is {this.state.serverState?.state} 
+                                  <Badge bg="success" style={{marginLeft:"10px"}} pill>{this.state.serverState?.players}</Badge></span>
+                                  </Link> : <Button variant="danger">Server is Offline</Button>}
 
-                 <ServerStatusModal show={this.state.showServerModal} serverState={this.state.serverState} toggleModal={()=>this.toggleServerModal()} />
+                                  <ServerStatusModal show={this.state.showServerModal} serverState={this.state.serverState} toggleModal={()=>this.toggleServerModal()} />
                                 </li>
                                   <li>Total Accounts: <b>-</b></li>
                                   <li>Total Characters: <b>-</b></li>
